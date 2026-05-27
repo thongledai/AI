745f00ceb7355d36eb7b2d430717a48c6b52da2b
thongledai <thongledai@gmail.com>
2026-05-28 00:52:36 +0700

    add file A*

---
+++ Algorithms_8Puzzle/AStar_8Puzzle.py
@@ -0,0 +1,9 @@
+import heapq
+from Support_Functions.CheckSolve import check_solve
+from Support_Functions.Node import *
+from Support_Functions.SupportFunctions import *
+from Support_Functions.CalculateCost import difference_cost, manhattan_cost
+
+# A*
+def AStar(start, goal):
+    pass
