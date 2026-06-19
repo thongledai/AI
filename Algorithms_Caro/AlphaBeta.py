7a3ef2604b66639ee79034b8359b5104ec9047fe
thongledai <thongledai@gmail.com>
2026-06-20 02:27:09 +0700

    add Caro game

---
+++ Algorithms_Caro/AlphaBeta.py
@@ -0,0 +1,19 @@
+def alpha_beta_minimax(state, depth, alpha, beta, maximizing_player):
+   if is_terminal(state) or depth == 0:
+       return utility(state)
+   if maximizing_player:
+       value = float('-inf')
+       for action in actions(state):
+           value = max(value, alpha_beta_minimax(result(state, action), depth-1, alpha, beta, False))
+           alpha = max(alpha, value)
+           if beta <= alpha:
+               break # Beta cut-off
+       return value
+   else:
+       value = float('inf')
+       for action in actions(state):
+           value = min(value, alpha_beta_minimax(result(state, action), depth-1, alpha, beta, True))
+           beta = min(beta, value)
+           if beta <= alpha:
+               break # Alpha cut-off
+       return value
---
+++ Algorithms_Caro/Minimax.py
@@ -0,0 +1,15 @@
+def minimax(state, depth, maximizing_player):
+   if is_terminal(state) or depth == 0:
+       return utility(state)
+   if maximizing_player:
+       max_eval = float('-inf')
+       for action in actions(state):
+           eval_score = minimax(result(state, action), depth - 1, False)
+           max_eval = max(max_eval, eval_score)
+       return max_eval
+   else:
+       min_eval = float('inf')
+       for action in actions(state):
+           eval_score = minimax(result(state, action), depth - 1, True)
+           min_eval = min(min_eval, eval_score)
+       return min_eval
